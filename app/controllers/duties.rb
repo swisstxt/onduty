5214479a9e725daddb3498c339566639c39e5362
niwo <nik.wolfgramm@gmail.com>
2017-02-28 18:56:28 +0100

    fix issue with flash and redirect in duties controller

--- app/controllers/duties.rb
+++ app/controllers/duties.rb
@@ -4,15 +4,20 @@ post '/duties/:id/:contact_id?' do
   protected!
   duty_type = params[:id].to_i
   Onduty::Contact.where(duty: duty_type).update_all(duty: 0)
-  contact = Onduty::Contact.find(params[:contact_id])
-  contact.update(duty: duty_type)
 
-  if Onduty::Notification.plugins.include? "SlackNotification"
-    post_slack_message(
-      ":+1: *#{contact.name}* is on duty (#{Onduty::Duty.types[duty_type]})."
-    )
+  begin
+    contact = Onduty::Contact.find(params[:contact_id])
+    contact.update(duty: duty_type)
+    if Onduty::Notification.plugins.include? "SlackNotification"
+      post_slack_message(
+        ":+1: *#{contact.name}* is on duty (#{Onduty::Duty.types[duty_type]} contact)."
+      )
+    end
+    flash[:success] = "Successfuly updated duty settings."
+  rescue
+    flash[:danger] = "ERROR: Couldn't associate duty with new contact: Contact not found."
   end
-
-  flash[:success] = "Successfuly updated duty settings."
-  redirect back
+  @title = "Contacts"
+  @contacts = Onduty::Contact.all.asc(:last_name)
+  erb :"contacts/index"
 end
