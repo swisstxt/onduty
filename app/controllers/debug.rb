4e596eda08920f5c91c5f53d85bdbed781fd13b1
Nik Wolfgramm <nik.wolfgramm@gmail.com>
2017-12-05 16:15:47 +0100

    adding debug info

--- app/controllers/debug.rb
+++ app/controllers/debug.rb
@@ -2,12 +2,20 @@
 get '/debug' do
   protected!
   @title = "Debug"
-  @settings = {}
+  @onduty_env = {}
   ENV.each do |k, v|
     if k.to_s.match(/^ONDUTY_.*/)
       if k.to_s.match(/(SECRET|PASSWORD)/)
         v = v == '' ? '' : '******'
       end
+      @onduty_env[k] = v
+    end
+  end
+  @settings = {}
+  Onduty::Config.new.settings.map do |k, v|
+    if k.to_s.match(/(secret|password)/)
+      @settings[k] = v == '' ? '' : '******'
+    else
       @settings[k] = v
     end
   end
--- lib/onduty/version.rb
+++ lib/onduty/version.rb
@@ -1,3 +1,3 @@
 module Onduty
-  VERSION = "1.1.10"
+  VERSION = "1.1.11"
 end
