a650e6e9940bc081977880d38c969ea021ebf85c
Nik Wolfgramm <nik.wolfgramm@gmail.com>
2018-03-08 20:11:40 +0100

    use unprivileged command for mongodb connection test

--- app/controllers/health.rb
+++ app/controllers/health.rb
@@ -2,9 +2,7 @@
 get '/_health.json' do
   content_type :json
   @status = {}
-  mongodb = Mongoid.client(:default).command(
-    serverStatus: 1, repl: 0, metrics: 0, locks: 0
-  )
+  mongodb = Mongoid.client(:default).command(connectionStatus: 1)
   @status[:mongodb] = (mongodb && mongodb.ok?) ? "OK" : "ERROR"
   twilio = JSON.parse(Net::HTTP.get(
     URI("https://gpkpyklzq55q.statuspage.io/api/v2/status.json")
